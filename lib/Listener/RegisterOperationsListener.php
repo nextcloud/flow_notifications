78d973202e8a599cdacccd8a1732a06ac5704675
falghamdi125 <152446244+falghamdi125@users.noreply.github.com>
2024-10-27 00:12:38 +0300

    fix(flow_notifications): Improve app loading by checking if notifications app is not installed and enabled when registering the app
    Signed-off-by: Faisal Alghamdi <falghamdi125@gmail.com>

--- lib/Listener/RegisterOperationsListener.php
+++ lib/Listener/RegisterOperationsListener.php
@@ -27,22 +27,29 @@ namespace OCA\FlowNotifications\Listener;
 
 use OCA\FlowNotifications\AppInfo\Application;
 use OCA\FlowNotifications\Flow\Operation;
+use OCP\App\IAppManager;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\Util;
 use OCP\WorkflowEngine\Events\RegisterOperationsEvent;
+use Psr\Log\LoggerInterface;
 
 /**
  * @template-implements IEventListener<Event|RegisterOperationsEvent>
  */
 class RegisterOperationsListener implements IEventListener {
 	public function __construct(
-		protected readonly Operation $operation
+		protected readonly Operation $operation,
+		protected IAppManager $appManager,
+		private readonly LoggerInterface $logger,
 	) {
 	}
 
 	public function handle(Event $event): void {
 		if (!$event instanceof RegisterOperationsEvent) {
+			return;
+		} elseif (!$this->appManager->isEnabledForUser('notifications')) {
+			$this->logger->error('Failed to register `flow_notifications` app. This could happen due to the `notifications` app isn\'t installed or enabled.', ['app' => 'flow_notifications']);
 			return;
 		}
 
