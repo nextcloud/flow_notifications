33a347920e6599a36e4a9f65f57be6dc4054353e
Arthur Schiwon <blizzz@arthur-schiwon.de>
2025-04-29 12:19:59 +0200

    fix: adjust to workflowengine changes and register web component
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- src/views/FlowNotify.vue
+++ src/views/FlowNotify.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
-		<input type="text"
+		<input v-model="currentInscription"
+			type="text"
 			maxlength="80"
 			:placeholder="placeholder"
-			:value="currentInscription"
 			@input="emitInput">
 	</div>
 </template>
@@ -14,11 +14,12 @@ export default {
 	name: 'FlowNotify',
 	components: {},
 	props: {
-		value: {
-			default: JSON.stringify({ inscription: '' }),
+		modelValue: {
+			default: '',
 			type: String,
 		},
 	},
+	emits: ['update:model-value'],
 	data() {
 		return {
 			inscription: '',
@@ -27,17 +28,18 @@ export default {
 	},
 	computed: {
 		currentInscription() {
-			if (!this.value) {
+			if (!this.modelValue) {
 				return ''
 			}
-			return JSON.parse(this.value).inscription
+			return JSON.parse(this.modelValue).inscription
 		},
 	},
 	methods: {
 		emitInput(value) {
-			if (value !== null) {
-				this.$emit('input', JSON.stringify({ inscription: value.target.value }))
+			if (value === null) {
+				return
 			}
+			this.$emit('update:model-value', JSON.stringify({ inscription: value.target.value }))
 		},
 	},
 }
