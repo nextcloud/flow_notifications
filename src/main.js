28a5defa7cfe547c43aa45645fc72cea37fdf0ea
Arthur Schiwon <blizzz@arthur-schiwon.de>
2025-04-30 16:49:35 +0200

    fix: add backwards compat for NC <= 31.0.2
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- src/main.js
+++ src/main.js
@@ -5,21 +5,27 @@
 
 import wrap from '@vue/web-component-wrapper'
 import Vue from 'vue'
+import semver from 'semver'
 
 import FlowNotify from './views/FlowNotify.vue'
 
-const FlowNotifyComponent = wrap(Vue, FlowNotify)
-const customElementId = 'oca-flow_notifications-operation-flow_notify'
-window.customElements.define(customElementId, FlowNotifyComponent)
+let customElementId
+// we may only compare first three numbers of the version
+if (semver.gt(window.OC.config.version.split('.', 3).join('.'), '31.0.2')) {
+	const FlowNotifyComponent = wrap(Vue, FlowNotify)
+	customElementId = 'oca-flow_notifications-operation-flow_notify'
+	window.customElements.define(customElementId, FlowNotifyComponent)
 
-// In Vue 2, wrap doesn't support disabling shadow :(
-// Disable with a hack
-Object.defineProperty(FlowNotifyComponent.prototype, 'attachShadow', { value() { return this } })
-Object.defineProperty(FlowNotifyComponent.prototype, 'shadowRoot', { get() { return this } })
+	// In Vue 2, wrap doesn't support disabling shadow :(
+	// Disable with a hack
+	Object.defineProperty(FlowNotifyComponent.prototype, 'attachShadow', { value() { return this } })
+	Object.defineProperty(FlowNotifyComponent.prototype, 'shadowRoot', { get() { return this } })
+}
 
 window.OCA.WorkflowEngine.registerOperator({
 	id: 'OCA\\FlowNotifications\\Flow\\Operation',
 	color: '#f1d340',
 	operation: '',
 	element: customElementId,
+	options: FlowNotify,
 })
